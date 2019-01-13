c86b20f67281276b660b0a32cede979378a90eec
WANG. YG <astwyg@126.com>
2019-01-13 10:07:59 +0800

    写完查找年报

---
+++ tools/fnd_keyword_in_annual_report.py
@@ -0,0 +1,17 @@
+import os
+
+path = "..\\data\\annual_report_2017_txt"
+
+def find_keyword(keyword):
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            with open(os.path.join(path, f),"r", encoding="utf-8") as fp:
+                content = fp.read()
+                keyword_times = content.count(keyword)
+                if keyword_times:
+                    print("{}在{}中出现{}次".format(keyword,f,keyword_times))
+
+
+if __name__ == "__main__":
+    keyword = input("输入关键字:")
+    find_keyword(keyword)
---
+++ utils/get_all_annual_report.py
@@ -0,0 +1,37 @@
+import time
+import tushare as ts
+import requests
+from bs4 import BeautifulSoup
+
+def get_all_stock_num():
+    ts.set_token('63fa7782075b5409355fef5dc288adc9bc7250fd5fb1bade38eb37bc')
+    pro = ts.pro_api()
+    data = pro.stock_basic()
+    all_stock = data['ts_code'].tolist()
+    return all_stock
+
+def download_annual_report(stock_num):
+    page1 = requests.get("http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_AllBulletin/stockid/{}.phtml?ftype=ndbg".format(stock_num))
+    page1.encoding = "utf-8"
+    page1 = BeautifulSoup(page1.text)
+    next_url = page1.find_all("div", class_="datelist")[0].contents[0].contents[1]["href"]
+    page2 = requests.get("http://vip.stock.finance.sina.com.cn/"+next_url)
+    page2.encoding = "gb2312"
+    page2 = BeautifulSoup(page2.text)
+    content = page2.find_all("div", class_="graybgH2")[0].contents[1]
+    name = page2.find_all("th",style="text-align:center")[0].contents[0].replace("\t","")
+    name = name.replace("*","").replace("?","")
+    with open("..\\data\\annual_report_2017_txt\\{}.txt".format(name),"w", encoding='utf-8') as f:
+        f.write(str(content))
+
+
+
+if __name__ == "__main__":
+    all_stock = get_all_stock_num()
+    for stock in all_stock:  # 改起点位置
+        try:
+            download_annual_report(stock.split(".")[0])
+        except IndexError:
+            print("{}没找到".format(stock))
+        time.sleep(12)
+        print("downloaded "+stock)
